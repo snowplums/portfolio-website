d5e324bf95b6effc3b0ff6e71f55dc05aecf3b64
mlh-localhost <waynemm14@gmail.com>
2022-10-01 18:24:36 -0700

    Starting to build website
    gaming

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div class="sidenav">
+        <div>
+          <p>
+            Hello! Welcome to my site!
+          </p>
+
+        </div>
+        <a href="./about">About</a>
+        <a href="./projects">Projects</a>
+        <a href="./links">Links</a>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
